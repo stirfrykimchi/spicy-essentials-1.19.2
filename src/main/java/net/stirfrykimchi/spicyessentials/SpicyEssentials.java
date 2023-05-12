d7a11435a428e63ca17e6d3ccf720d709d04fcf3
stirfrykimchi <donkeygottapoo@gmail.com>
2023-05-12 13:07:42 -1000

    Initial commit

---
+++ src/main/java/net/stirfrykimchi/spicyessentials/SpicyEssentials.java
@@ -0,0 +1,15 @@
+package net.stirfrykimchi.spicyessentials;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SpicyEssentials implements ModInitializer {
+	public static final String MOD_ID = "spicyessentials";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// e
+	}
+}
---
+++ src/main/java/net/stirfrykimchi/spicyessentials/SpicyEssentialsClient.java
@@ -0,0 +1,10 @@
+package net.stirfrykimchi.spicyessentials;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpicyEssentialsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
