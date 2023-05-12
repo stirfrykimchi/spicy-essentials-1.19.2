746c4b2c8fe03a3633ed9b7f2bb492e33b8ff40d
stirfrykimchi <donkeygottapoo@gmail.com>
2023-05-12 13:08:57 -1000

    Test Commit

--- src/main/java/net/stirfrykimchi/spicyessentials/SpicyEssentialsClient.java
+++ src/main/java/net/stirfrykimchi/spicyessentials/SpicyEssentialsClient.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class SpicyEssentialsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        //e
     }
 }
